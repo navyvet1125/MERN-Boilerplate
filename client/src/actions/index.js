8e63802873788cd8f131ef312e6d47907029ed2f
Evan J. Washington <enavy04@gmail.com>
2018-12-18 00:31:54 -0800

    added persistent login functionality

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import { AUTH_USER, AUTH_ERR } from './types'
 
-export const signup = formProps => async dispatch => {
+export const signup = (formProps, callback) => async dispatch => {
   try {
     const response = await axios.post('http://localhost:3090/signup', formProps)
     dispatch({ type: AUTH_USER, payload: response.data.token  })
+    localStorage.setItem('token', response.data.token)
+    callback()
   } catch (err){
     dispatch({ type: AUTH_ERR, payload: err.response.data})
   }
