205d8af595ce3eb09b90c76c6263a5ce9f6cadc5
Evan J. Washington <enavy04@gmail.com>
2018-12-18 07:48:30 -0800

    built out sign in and sign out features as well as added a little bit of css to links

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -11,3 +11,20 @@ export const signup = (formProps, callback) => async dispatch => {
     dispatch({ type: AUTH_ERR, payload: err.response.data})
   }
 }
+
+export const signin = (formProps, callback) => async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:3090/signin', formProps)
+    dispatch({ type: AUTH_USER, payload: response.data.token  })
+    localStorage.setItem('token', response.data.token)
+    callback()
+  } catch (err){
+    dispatch({ type: AUTH_ERR, payload: err.response.data})
+  }
+}
+
+
+export const signout = () => {
+  localStorage.removeItem('token')
+  return { type: AUTH_USER, payload: ''}
+}
